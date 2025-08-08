7e93b7445f8696af7532e0ed2396c11d39d843f4
user.email <yuriglauco945@gmail.com>
2025-08-08 15:02:57 -0300

    PostCreate refactored to resolve error 422 due owner_id and PostUpdate should not be able to change the owner_id

--- app/schemas/post.py
+++ app/schemas/post.py
@@ -4,12 +4,12 @@ from typing import Optional
 class PostBase(BaseModel):
     title: str
     text: str
-    owner_id: int
 
     model_config = ConfigDict(from_attributes = True)
 
 class PostOut(PostBase):
     id: int
+    owner_id: int
 
 class PostCreate(PostBase):
     pass
@@ -17,6 +17,5 @@ class PostCreate(PostBase):
 class PostUpdate(BaseModel):
     title: Optional[str] = None
     text: Optional[str] = None
-    owner_id: Optional[int] = None
 
     model_config = ConfigDict(from_attributes = True)
