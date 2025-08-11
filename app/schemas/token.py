b59c866ffbe4e17058f1d3bbfa375dec25c93c07
user.email <yuriglauco945@gmail.com>
2025-08-11 16:08:56 -0300

    created class TokenData on schemas 'token.py'

--- app/schemas/token.py
+++ app/schemas/token.py
@@ -1,6 +1,10 @@
 from pydantic import BaseModel
+from typing import Optional
 
 # define a estrutura de resposta como UserOut ou PostOut porém usado em endpoints que retorne um Token
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+class TokenData(Token):
+    email: Optional[str] = None
